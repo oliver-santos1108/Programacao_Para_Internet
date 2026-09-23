71bfb723e52f938166859d878f7763c28511d2ef
oliver-santos1108 <olivercs1108@gmail.com>
2026-09-23 09:41:51 -0300

    aula 23/09

---
+++ js/1.js
@@ -0,0 +1,18 @@
+console.log("Olá");
+
+//Declarar variaveis
+i=0; //variavel global var
+var nome = "Oliver"; //variavel global
+nome = 1;
+let numero; //variavel local po bloco
+const numeroPI = 3.141516;
+
+console.log("Variavel i: "+typeof(i));
+console.log("Variavel nome: "+typeof(nome));
+//===tipos de variaveis===
+//undefined
+//number
+//String
+//object
+//null
+
